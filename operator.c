48726ae60a08b1f1ebac8313b30c2c81b42ecaf1
Jayapriya.B <jayapriyabalasubramaniyan@gmail.com>
2026-08-26 17:38:21 +0530

    Create operator.c with operator examples
    Added basic arithmetic and assignment operators demonstration.

---
+++ operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+int x=5,y=6;
+printf("%d",x+y); // Arithmetic operator
+printf("%d",x+=5); // Assignment operator
+printf("%d",x-=5);
+printf("%d",x/=5);
+return 0;
+}
